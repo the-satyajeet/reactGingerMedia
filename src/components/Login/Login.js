72bc144968bfd50758172c7bfbbd599c0a792a6c
Satyajeet Gaur <the.satyajeetgaur@gmail.com>
2024-02-09 16:10:25 +0530

    update

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./login.module.css"
 import InputControl from "../InputControl/InputControl"
+import {Link} from "react-router-dom"
 
 function Login() {
   return (
@@ -8,6 +9,17 @@ function Login() {
       <div className={styles.innerBox}>
         <h1 className={styles.heading}>Login</h1>
         <InputControl label="User Name" placeholder="Enter email address"/>
+        <InputControl label="Password" placeholder="Enter password" />
+
+        <div className={styles.footer}>
+            <button>Login</button>
+            <p>
+                New here?{" "}
+                <span>
+                    <Link to="/signup">Sign up</Link>
+                </span>
+            </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,87 @@
-import React from 'react'
+import React from "react";
+import styles from "./Signup.module.css";
+import InputControl from "../InputControl/InputControl";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 
 function Signup() {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    pass: "",
+  });
+
+  const [errorMsg, setErrorMsg] = useState("");
+  const [ passMsg, setPassMsg ] = useState("");
+  const [submitBtnDisabled, setSubmitBtnDisabled] = useState(false);
+
+  const handleSubmit = () => {
+    if (!values.name || !values.email || !values.pass) {
+      setErrorMsg("Please fill all the fields");
+      return;
+    }
+    setErrorMsg("");
+    setSubmitBtnDisabled(true);
+    createUserWithEmailAndPassword(auth, values.email, values.pass)
+      .then((res) => {
+        setSubmitBtnDisabled(false);
+          console.log(res);
+      })
+      .catch((err) => {
+        setSubmitBtnDisabled(false);
+        console.log("Error: ", err.message);
+        const errCode = err.message;
+        setPassMsg(`${errCode}`);
+
+      });
+    console.log(values);
+  };
+
   return (
-    <div>
-      Signup
+    <div className={styles.container}>
+      <div className={styles.innerBox}>
+        <h1 className={styles.heading}>Signup</h1>
+        <InputControl
+          label="Name"
+          placeholder="Enter Name"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, name: event.target.value }))
+          }
+        />
+
+        <InputControl
+          label="Email"
+          placeholder="Enter email address"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, email: event.target.value }))
+          }
+        />
+
+        <InputControl
+          label="Password"
+          placeholder="Enter password"
+          onChange={(event) =>
+            setValues((prev) => ({ ...prev, pass: event.target.value }))
+          }
+        />
+        <b className={styles.error}>{passMsg}</b>
+
+        <div className={styles.footer}>
+          <b className={styles.error}>{errorMsg}</b>
+          <button onClick={handleSubmit} disabled = {submitBtnDisabled}>Sign up</button>
+          <p>
+            Already have an account?{" "}
+            <span>
+              <Link to="/login">Login</Link>
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
